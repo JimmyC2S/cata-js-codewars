6348485fea4a90e912b1ace141890fbb804539e8
jimmy <jimmy@creation2sites.fr>
2023-06-05 12:50:41 +0200

    reurn.js

---
+++ reurn negative.js
@@ -0,0 +1,8 @@
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+function makeNegative(num) {
+    if ( num < 0){
+      return num;
+      }
+      else {
+        return -num }
+     }
